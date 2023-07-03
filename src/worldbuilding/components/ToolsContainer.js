c417a91d6d564d87ca3aa0a5b0d6b8fe65f6d90a
Dean-Baylem <dbaylem5822@gmail.com>
2023-07-03 16:55:21 +0900

    Empty location for video changed to static image on DM Tools section - Restyle and rearrange the DM Tools section to match new style

--- src/worldbuilding/components/ToolsContainer.js
+++ src/worldbuilding/components/ToolsContainer.js
@@ -1,38 +1,43 @@
 import React from "react";
 import SideCard from "../../shared/Components/UIComponents/SideCard";
-import { Button } from "@mui/material";
 import "./ToolsContainer.css";
 
 const ToolsContainer = (props) => {
-  
-
   return (
     <div className="tools-container">
-      <div className="tool-display-vid-container">
-        <h5 className="page-subtitle">
-          Enhance your GM Experience with these handy tools!
-        </h5>
-        <p>This is where the video will go!</p>
-      </div>
-      <div className="tool-text-desc page-body">
-        <p>
-          As a fellow Dungeon Delver, you have access to an ever expanding range
-          of tools available to enchance the game experience for yourself and
-          your players.
-        </p>
-        <p>
-          Feedback is always welcome at <strong>Dungeon Delver HQ</strong> to
-          refine each tool to provide the best experience possible.
-        </p>
-        {/* The button below will bring up a modal with the feedback form */}
-        <Button>Feedback</Button>
+      <div className="tool-display-title">
+        <div className="tool-desc">
+          <h5 className="page-subtitle">
+            Enhance your GM Experience with these handy tools!
+          </h5>
+          <div className="tool-text-desc page-body">
+            <p>
+              As a fellow Dungeon Delver, you have access to an ever expanding
+              range of tools available to enchance the game experience for
+              yourself and your players.
+            </p>
+            <p>
+              Feedback is always welcome at <strong>Dungeon Delver HQ</strong>{" "}
+              to refine each tool to provide the best experience possible.
+            </p>
+          </div>
+        </div>
+        <div className="tools-token">
+          <img src="https://cdn-icons-png.flaticon.com/512/7231/7231328.png" />
+        </div>
       </div>
       <div className="tool-card-display-container">
         {props.currentTools.map((tool, index) => (
-            <SideCard key={index} link={tool.link} clickable={true} imgSrc={tool.imgSrc} cardType="top-side">
-              <h5 className="tool-card-title page-subtitle">{tool.title}</h5>
-              <p className="tool-card-body page-body">{tool.content}</p>
-            </SideCard>
+          <SideCard
+            key={index}
+            link={tool.link}
+            clickable={true}
+            imgSrc={tool.imgSrc}
+            cardType="top-side"
+          >
+            <h5 className="tool-card-title page-subtitle">{tool.title}</h5>
+            <p className="tool-card-body page-body">{tool.content}</p>
+          </SideCard>
         ))}
       </div>
     </div>
